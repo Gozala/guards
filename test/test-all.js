53bd2edb12bef06f1725101efc470dfe3ac935b0
Irakli Gozalishvili <rfobic@gmail.com>
2011-03-02 15:35:05 +0100

    Adding few more String guard tests.

--- test/test-all.js
+++ test/test-all.js
@@ -3,15 +3,23 @@
 var guards = require("guards");
 
 exports["test string guard"] = function(assert) {
-  var g = guards.String("default");
-  assert.equal(g(), "default", "correct default value is assigned");
-  assert.equal(g("foo"), "foo", "string guard accepts string values");
+  var g1 = guards.String("default");
+  assert.equal(g1(), "default", "correct default value is assigned");
+  assert.equal(g1("foo"), "foo", "string guard accepts string values");
   assert.throws(function() {
-    var value = g(12);
+    var value = g1(12);
   }, /String/g, "non-string passed to a string gaurd throws a TypeError");
   assert.throws(function() {
-    var value = g(new String("foo bar"));
+    var value = g1(new String("foo bar"));
   }, /String/g, "String instance throws exception as well");
+
+  var g2 = guards.String();
+  assert.strictEqual(g2(), undefined, "`undefined` is fallback for default");
+
+  var g3 = guards.String("", "Boom -> {{value}}!");
+  assert.throws(function() {
+    var value = g3({});
+  }, /Boom \-\> \[object Object\]!/g, "Optional error temaplaet can be used");
 };
 
 
