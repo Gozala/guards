e0f9871edd0484f515afc01d68743f16a265d1f2
Irakli Gozalishvili <rfobic@gmail.com>
2011-03-02 19:12:55 +0100

    Adding comments and tests for a schema guards.

--- test/test-all.js
+++ test/test-all.js
@@ -22,5 +22,58 @@ exports["test string guard"] = function(assert) {
   }, /Boom \-\> \[object Object\]!/g, "Optional error temaplate can be used");
 };
 
+exports["test number guard"] = function(assert) {
+  var g1 = guards.Number(0);
+  assert.equal(g1(), 0, "correct default value is assigned");
+  assert.equal(g1(17), 17, "string guard accepts string values");
+  assert.throws(function() {
+    g1("12");
+  }, /Number/g, "non-number throws a TypeError");
+
+  var g2 = guards.Number();
+  assert.strictEqual(g2(), undefined, "`undefined` is fallback for default");
+
+  var g3 = guards.Number(0, "number expected not a {{type}}");
+  assert.throws(function() {
+    g3("boom!");
+  }, /number expected not a string/g, "Optional error temaplate can be used");
+};
+
+exports["test schema guard"] = function(assert) {
+  var Point = guards.Schema({
+    x: guards.Number(0),
+    y: guards.Number(0)
+  });
+
+  assert.deepEqual(Point(), { x: 0, y: 0 }, "default values are insterted");
+  assert.deepEqual(Point({ x: 17, z: 50 }), { x: 17, y: 0 },
+                   "non-guarded values are stripped out");
+  assert.throws(function() {
+    Point({ x: "5" });
+  }, /Number/g, "Wrong type in caught by nested guard");
+
+  assert.throws(function() {
+    Point("{ y: 6 }");
+  }, /Object/g, "Scheme guard throws on non-object values");
+
+
+  var Segment = guards.Schema({
+    start: Point,
+    end: Point,
+    opacity: guards.Number(1)
+  });
+
+  assert.deepEqual(Segment(),
+                  { start: { x: 0, y: 0 }, end: { x: 0, y: 0 }, opacity: 1 },
+                  "default values are used by all nested guards");
+
+  assert.deepEqual(Segment({ end: { x: 17, foo: 'bar' }, opacity: 0.5 }),
+                  { start: { x: 0, y: 0 }, end: { x: 17, y: 0 }, opacity: 0.5 },
+                  "fallback to default values and strip out non-guarded ones");
+
+  assert.throws(function() {
+    Segment({ start: 17 })
+  }, /Object/g, "nested guard throw on wrong value types");
+};
 
 require("test").run(exports)
